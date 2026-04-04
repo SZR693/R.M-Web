622cc6499dac07f80e6b91601e3159c00486d90b
SZR693 <contact.rmwebdesign@gmail.com>
2026-04-05 01:44:36 +0200

    Fix

--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -1,14 +1,14 @@
 /// <reference types="vite/client" />
+
 declare module '*.css';
 
-declare module '*.css' {
-  const content: { [className: string]: string };
-  export default content;
-}
-
 interface ImportMetaEnv {
+  // Clé Cloudflare Turnstile
   readonly VITE_CLOUDFLARE_TURNSTILE_SITE_KEY: string;
-
+  
+  // Clés Supabase (Ajoute celles-ci)
+  readonly VITE_SUPABASE_URL: string;
+  readonly VITE_SUPABASE_ANON_KEY: string;
 }
 
 interface ImportMeta {
